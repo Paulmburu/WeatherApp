a6589a7a1191b6c687118d1cf522cd0269caaee0
Paulmburu <paulmburu53@gmail.com>
2022-03-23 09:01:53 +0300

    chore: code coverage

--- data/local/build.gradle.kts
+++ data/local/build.gradle.kts
@@ -3,7 +3,13 @@ plugins {
     id("kotlin-android")
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+    id(Libraries.Jacoco.jacocoAndroid)
 }
+
+jacoco {
+    toolVersion = Libraries.Jacoco.version
+}
+
 
 android {
     defaultConfig {
